81305e4073b30d9ccfcffee17301873053dffdf1
Thomas Gelf <thomas@gelf.net>
2019-11-14 16:36:08 +0100

    IcingaController: allow to post status

--- application/controllers/IcingaController.php
+++ application/controllers/IcingaController.php
@@ -8,6 +8,10 @@ class IcingaController extends ControllerBase
 {
     public function statusAction()
     {
+        if ($this->getRequest()->isPost()) {
+            $this->postStatus();
+            return;
+        }
         $object = $this->requireObjectParameter();
         try {
             $result = $this->ido()->getObjectState($object);
@@ -21,6 +25,38 @@ class IcingaController extends ControllerBase
                 echo $tag;
                 $this->finish();
             }
+        } catch (\Exception $e) {
+            $this->fail(500, $e->getMessage());
+        }
+    }
+
+    protected function postStatus()
+    {
+        $request = $this->getRequest();
+        $object = $request->getPost('Object');
+        $state = $request->getPost('State');
+        $message = $request->getPost('Message');
+        $path = $request->getPost('Path');
+        if ($object === null) {
+            $this->fail(400, "Parameter 'Object' is missing");
+        }
+        if ($state === null) {
+            $this->fail(400, "Parameter 'State' is missing");
+        }
+        if ($message === null) {
+            $this->fail(400, "Parameter 'Message' is missing");
+        }
+        try {
+            $result = $this->ido()->hasObject($object);
+            $tag = Html::tag('result')->setSeparator("\r\n");
+            if ($result === false) {
+                $tag->add(Html::tag('success', 'true'));
+            } else {
+                $tag->add(Html::tag('success', 'false'));
+                $tag->add(Html::tag('reason', 'Object not found'));
+            }
+            echo $tag;
+            $this->finish();
         } catch (\Exception $e) {
             $this->fail(500, $e->getMessage());
         }
