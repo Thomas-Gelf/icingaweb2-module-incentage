668566d6c5dff2a2c4118985a78f1cb83511f2e1
Thomas Gelf <thomas@gelf.net>
2019-11-14 21:41:19 +0100

    DirectorController: run Sync and provide stats

--- application/controllers/DirectorController.php
+++ application/controllers/DirectorController.php
@@ -6,12 +6,15 @@ use Icinga\Application\Config;
 use Icinga\Application\Icinga;
 use Icinga\Module\Director\Db;
 use Icinga\Module\Director\Objects\ImportSource as DirectorImportSource;
+use Icinga\Module\Director\Objects\SyncRule;
 use Icinga\Module\Incentage\ProvidedHook\Director\ImportSource;
 use ipl\Html\Html;
-use SimpleXMLElement;
 
 class DirectorController extends ControllerBase
 {
+    /** @var Db */
+    protected $directorDb;
+
     /**
      * @throws \Icinga\Exception\ProgrammingError
      */
@@ -34,26 +37,98 @@ class DirectorController extends ControllerBase
         }
 
         try {
-            $result = Html::tag('result');
+            $result = Html::tag('result')->setSeparator("\r\n");;
             $importSource = $this->requireImportSource();
             $this->importXml($request->getRawBody());
+            $sync = $this->eventuallyLoadSync($importSource);
             if ($importSource->runImport()) {
                 $result->add([
                     Html::tag('success', 'true'),
-                    Html::tag('changes', 'true'),
+                    Html::tag('importedChanges', 'true'),
                     Html::tag('message', 'Modified objects have been imported'),
                 ]);
             } else {
                 $result->add([
                     Html::tag('success', 'true'),
-                    Html::tag('changes', 'false'),
+                    Html::tag('importedChanges', 'false'),
                     Html::tag('message', 'Nothing has been changed, imported data is still up to date'),
                 ]);
+            }
+
+            if ($sync) {
+                $result->add(Html::tag('syncRuleFound', 'true'));
+                $mods = $this->getExpectedModificationCounts($sync);
+                if ($sync->applyChanges()) {
+                    $result->add(Html::tag('synchronizedChanges', 'true'));
+                    foreach ($mods as $key => $value) {
+                        $result->add(Html::tag('objects' . \ucfirst($key), $value));
+                    }
+                } else {
+                    $result->add(Html::tag('synchronizedChanges', 'false'));
+                }
+            } else {
+                $result->add(Html::tag('syncRuleFound', 'false'));
             }
             echo $result;
             $this->finish();
         } catch (\Exception $e) {
             $this->fail(400, $e->getMessage());
+        }
+    }
+
+    protected function getExpectedModificationCounts(SyncRule $rule)
+    {
+        $modifications = $rule->getExpectedModifications();
+
+        $create = 0;
+        $modify = 0;
+        $delete = 0;
+
+        /** @var \Icinga\Module\Director\Objects\IcingaObject $object */
+        foreach ($modifications as $object) {
+            if ($object->hasBeenLoadedFromDb()) {
+                if ($object->shouldBeRemoved()) {
+                    $delete++;
+                } else {
+                    $modify++;
+                }
+            } else {
+                $create++;
+            }
+        }
+
+        return (object) [
+            'created' => $create,
+            'modified' => $modify,
+            'deleted' => $delete,
+        ];
+    }
+
+    /**
+     * @param DirectorImportSource $importSource
+     * @return SyncRule|null
+     * @throws \Icinga\Exception\NotFoundError
+     */
+    protected function eventuallyLoadSync(DirectorImportSource $importSource)
+    {
+        $ddb = $this->requireDirectorDb();
+        $db = $ddb->getDbAdapter();
+        $ids = $db->fetchCol(
+            $db->select()->distinct()->from(
+                ['sr' => 'sync_rule'],
+                'sr.id'
+            )->join(
+                ['sp' => 'sync_property'],
+                'sr.id = sp.rule_id',
+                []
+            )->where('sp.source_id = ?', $importSource->get('id'))
+        );
+
+        if (\count($ids) === 1) {
+            $id = array_shift($ids);
+            return SyncRule::loadWithAutoIncId($id, $ddb);
+        } else {
+            return null;
         }
     }
 
@@ -67,12 +142,22 @@ class DirectorController extends ControllerBase
         if ($importSourceName === null) {
             $this->fail(401, 'No access to a Director Import Source has been granted');
         }
-        $directorResourceName = Config::module('director')->get('db', 'resource');
 
         return DirectorImportSource::loadByName(
             $importSourceName,
-            Db::fromResourceName($directorResourceName)
+            $this->requireDirectorDb()
         );
+    }
+
+    protected function requireDirectorDb()
+    {
+        if ($this->directorDb === null) {
+            $directorResourceName = Config::module('director')->get('db', 'resource');
+
+            $this->directorDb = Db::fromResourceName($directorResourceName);
+        }
+
+        return $this->directorDb;
     }
 
     protected function importXml($string)
